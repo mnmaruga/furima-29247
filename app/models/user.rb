b652ecec10f414e34f4a4cd41c9aa0f5658fc334
mnmaruga <toi_moi.em@icloud.com>
2020-09-03 19:06:55 +0900

    カラムの追加とバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :sales
+  
+  validates :name,            presence: true
+  validates :password,                       format: {with: /\A[a-z\d]+\z/}
+  validates :first_name,      presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name,       presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name_kana, presence: true,format: {with: /\A[ァ-ン]+\z/}
+  validates :last_name_kana,  presence: true,format: {with: /\A[ァ-ン]+\z/}
+  validates :birth,           presence: true
 end
