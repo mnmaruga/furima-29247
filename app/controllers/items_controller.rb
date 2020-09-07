e3f1e5c0df859e5a7f9fb7b58290b59df5a30a39
mnmaruga <toi_moi.em@icloud.com>
2020-09-07 14:46:02 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,10 @@ class ItemsController < ApplicationController
   def new
     @items = Item.new
   end
+
+  private
+
+  # def item_params
+  #   params.require(:item).permit(:item, :detail, :image).merge(user_id: current_user.id)
+  # end
 end
