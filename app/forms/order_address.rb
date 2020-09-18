fc6d71de119871ee20a00df51cc320b7876de590
mnmaruga <toi_moi.em@icloud.com>
2020-09-18 17:42:37 +0900

    修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -3,16 +3,15 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :token, :postal, :prefect_id, :city, :address, :building, :tel, :price, :order_id
 
   with_options presence: true do
-    validates :postal, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'code input correctly' }
-    validates :prefect_id
+    validates :postal,     format: { with: /\A\d{3}[-]\d{4}\z/, message: 'code input correctly' }
+    validates :prefect_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
     validates :address
-    validates :tel,    format: { with: /\d/, message: 'Half-width number' },
-                       length: { maximum: 11, message: 'is invalid' }
+    validates :tel,        format: { with: /\d/, message: 'Half-width number' },
+                           length: { maximum: 11, message: 'is invalid' }
     validates :token
   end
-  validates :prefect_id, numericality: { other_than: 1, message: 'Select' }
-
+  
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(item_id: item_id, postal: postal, prefect_id: prefect_id, city: city, address: address, building: building, tel: tel, order_id: order.id)
